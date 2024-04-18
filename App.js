3d3a3591ecc3c1f42f72d52feb1210e56a3c56b2
wilson-lin0 <wilsonlinwilin@gmail.com>
2024-04-18 09:05:05 -0400

    a6 Mongo su23

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import express from "express";
+import mongoose from "mongoose";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: [process.env.FRONTEND_URL, "http://localhost:3000"]
+}));
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json())
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
